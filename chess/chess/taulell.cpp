f50f53589bf5e9f3f8d62e4c368610fa62a06228
m4rtin <martin.pastor@estudiant.enti.cat>
2025-12-01 10:46:01 +0100

    no message

--- chess/chess/taulell.cpp
+++ chess/chess/taulell.cpp
@@ -4,12 +4,7 @@
 char taulell[TAMANYTAULELL][TAMANYTAULELL];
 
 void generarTaulell() {
-    for (int fila = 0; fila < TAMANYTAULELL; fila++) {
-        for (int col = 0; col < TAMANYTAULELL; col++)
-        {
-            taulell[fila][col] = '~';
-            std::cout << taulell[fila][col] << ' ';
-        }
+    
         std::cout << "\n";
-    }
+    
 }
